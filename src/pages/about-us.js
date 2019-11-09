154a1a511f8f7bd4f9bbf4b28d207f9436bc1d12
Mateusz Szuber <mati@MacBook-Air-Mateusz-2.local>
2019-11-09 08:55:23 +0100

    about us

--- src/pages/about-us.js
+++ src/pages/about-us.js
@@ -1,10 +1,53 @@
 import React from "react"
+import styled from 'styled-components';
 import Header from "../components/Header"
 import Nav from "../components/Nav"
 import Footer from "../components/Footer/Footer"
-import Content from "../components/Content"
-import Cards from "../components/Cards"
 import GlobalStyle from "../components/GlobalStyles/GlobalStyles"
+import AboutUsCard from '../components/AboutUsCard/AboutUsCard'
+
+const aboutUsData = [
+    {
+        imgSrc: 'https://via.placeholder.com/150',
+        fullName: 'Wojciech John',
+        description: 'Frontendowiec z zamiłowania'
+    },
+    {
+        imgSrc: 'https://via.placeholder.com/150',
+        fullName: 'Wojciech John2',
+        description: 'Drugi opis'
+    },
+    {
+        imgSrc: 'https://via.placeholder.com/150',
+        fullname: 'Wojciech John3',
+        description: 'Trzeci opis',
+    },
+    {
+        imgSrc: 'https://via.placeholder.com/150',
+        fullName: 'Wojciech John2',
+        description: 'Drugi opis'
+    },
+    {
+        imgSrc: 'https://via.placeholder.com/150',
+        fullName: 'Wojciech John2',
+        description: 'Drugi opis'
+    },
+    {
+        imgSrc: 'https://via.placeholder.com/150',
+        fullName: 'Wojciech John2',
+        description: 'Drugi opis'
+    },
+]
+
+const Container = styled.div`
+    display: block;
+    max-width: 1100px;
+    padding: 0 1rem;
+    margin: 5rem auto 0rem;
+    @media screen and (max-width: 600px) {
+        margin: 2.5rem auto 0rem;
+  }
+`
 
 const IndexPage = () => {
     return (
@@ -12,8 +55,9 @@ const IndexPage = () => {
             <GlobalStyle />
             <Nav />
             <Header />
-            <Content />
-            <Cards />
+            <Container>
+                <AboutUsCard aboutUsData={aboutUsData} />
+            </Container>
             <Footer />
         </>
     )
