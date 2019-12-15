86563bfdcc5ed3a3fc688d5e32c6991d28bc1644
johnywojtek <johnywojtek@gmail.com>
2019-12-15 12:19:49 +0100

    Updates

---
+++ component---src-pages-404-js-84a3fe3bbf20d5647c36.js
@@ -0,0 +1,2 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[2],{242:function(e,t,n){"use strict";n.r(t);var a=n(0),u=n.n(a);t.default=function(){return u.a.createElement("div",null,u.a.createElement("h1",null,"NOT FOUND"),u.a.createElement("p",null,"You just hit a route that doesn't exist... the sadness."))}}}]);
+//# sourceMappingURL=component---src-pages-404-js-84a3fe3bbf20d5647c36.js.map
